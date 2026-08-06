4e8ef0be0c1358f46301377b28bc784b5088e309
emmanuelobana90-hash <emmanuelobana90@gmail.com>
2026-08-06 08:28:31 +0100

    Create  layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Yaoundé Propre',
+  description: 'Application de signalement citoyen',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
